881f02467701390e431b4d466c6fbef11cf252fe
Alexander Podkopaev <p-alex1990@mail.ru>
2020-07-07 17:30:06 +0700

    change Data

--- src/main/kotlin/com/podkopaev/alexander/itsm/Webhook.kt
+++ src/main/kotlin/com/podkopaev/alexander/itsm/Webhook.kt
@@ -3,7 +3,6 @@ package com.podkopaev.alexander.itsm
 import com.justai.jaicf.channel.http.httpBotRouting
 import com.justai.jaicf.channel.telegram.TelegramChannel
 import com.justai.jaicf.channel.yandexalice.AliceChannel
-import com.podkopaev.alexander.itsm.naumen.NaumenData.OAUTH_TOKEN
 import com.podkopaev.alexander.itsm.naumen.NaumenData.TELEGRAM_TOKEN
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
