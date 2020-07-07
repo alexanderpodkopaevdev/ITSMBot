9a2c86c4b2d254cffa349a3b9974ee03014c7556
Alexander Podkopaev <p-alex1990@mail.ru>
2020-07-07 18:08:02 +0700

    change data

--- src/main/kotlin/com/podkopaev/alexander/itsm/Telegram.kt
+++ src/main/kotlin/com/podkopaev/alexander/itsm/Telegram.kt
@@ -3,6 +3,6 @@ package com.podkopaev.alexander.itsm
 import com.justai.jaicf.channel.telegram.TelegramChannel
 import com.podkopaev.alexander.itsm.naumen.NaumenData
 
-fun main() {
-    TelegramChannel(skill, NaumenData.TELEGRAM_TOKEN).run()
-}
+    fun main() {
+        TelegramChannel(skill, NaumenData.TELEGRAM_TOKEN).run()
+    }
