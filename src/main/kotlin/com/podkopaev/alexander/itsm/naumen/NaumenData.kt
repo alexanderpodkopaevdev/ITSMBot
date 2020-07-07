24601d78f5452c471ff18d54912ce45611c05147
Alexander Podkopaev <p-alex1990@mail.ru>
2020-07-07 17:17:45 +0700

    change Data

---
+++ src/main/kotlin/com/podkopaev/alexander/itsm/naumen/NaumenData.kt
@@ -0,0 +1,7 @@
+package com.podkopaev.alexander.itsm.naumen
+
+object NaumenData {
+    val SERVER_URL = System.getenv("SERVER_URL")
+    val ACCESS_KEY = System.getenv("ACCESS_KEY")
+    val TELEGRAM_TOKEN = System.getenv("TELEGRAM_TOKEN")
+}
