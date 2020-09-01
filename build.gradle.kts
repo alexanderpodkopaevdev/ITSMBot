7b75a097174acd74f9f4c8b8848c3cfcc7528a51
Alexander Podkopaev <p-alex1990@mail.ru>
2020-09-01 17:26:57 +0500

    change buildgradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val slf4j = "1.7.30"
 val ktor = "1.3.1"
 
 application {
-    mainClassName = "com.podkopaev.alexander.itsm.Webhook.kt"
+    mainClassName = "com.podkopaev.alexander.itsm.WebhookKt"
 }
 
 repositories {
