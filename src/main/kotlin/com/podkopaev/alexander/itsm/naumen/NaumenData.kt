191b45aa91f69e18cb6d48a8bb2c2ad441dae626
Alexander Podkopaev <p-alex1990@mail.ru>
2020-09-01 16:35:25 +0500

    add alice

--- src/main/kotlin/com/podkopaev/alexander/itsm/naumen/NaumenData.kt
+++ src/main/kotlin/com/podkopaev/alexander/itsm/naumen/NaumenData.kt
@@ -4,4 +4,5 @@ object NaumenData {
     val SERVER_URL = System.getenv("SERVER_URL")// ?: NaumenDataHide.SERVER_URL
     val ACCESS_KEY = System.getenv("ACCESS_KEY") //?: NaumenDataHide.ACCESS_KEY
     val TELEGRAM_TOKEN = System.getenv("TELEGRAM_TOKEN") //?: NaumenDataHide.TELEGRAM_TOKEN
+    val OAUTH_TOKEN = System.getenv("OAUTH_TOKEN") //?: NaumenDataHide.OAUTH_TOKEN
 }
